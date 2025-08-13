55031f6fd707b82047c9e3b3d5b55c1a3df35250
Miguel Jesus <84322271+migueljesus679@users.noreply.github.com>
2025-08-13 08:56:26 +0000

    updates na renderização

--- src/Faturas.js
+++ src/Faturas.js
@@ -489,7 +489,7 @@ export default function Faturas() {
 
       <AnimatePresence mode="wait">
         <motion.div
-          key={paginaAtual}
+          key={`${paginaAtual}-${faturas.length}`}
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           exit={{ opacity: 0, y: -20 }}
